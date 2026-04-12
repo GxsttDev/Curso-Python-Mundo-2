84423beda3cf86345ada8765f684b7e008aea0f7
GxsttDev <gustavogomes3035@gmail.com>
2026-04-12 00:28:49 -0300

    Atualizar ex037

--- ex037.py
+++ ex037.py
@@ -6,10 +6,10 @@ print ("[3] HEXADECIMAL ")
 opcao = int(input("QUAL SUA OPÇÃO: "))
 
 if opcao == 1:
-    print (f"{numero} em binario é igual a: {bin(numero)}")
+    print (f"{numero} em binario é igual a: {bin(numero)[2:]}")
 elif opcao == 2:
-    print (f"{numero} em octal é igual a: {oct(numero)}")
+    print (f"{numero} em octal é igual a: {oct(numero)[2:]}")
 elif opcao == 3: 
-    print (f"{numero} em hexadecimal é igual a: {hex(numero)}")
+    print (f"{numero} em hexadecimal é igual a: {hex(numero)[2:]}")
 else:
-    print (f"OPÇÃO INVALIDA, escolha [1] , [2] ou [3]")
+    print (f"OPÇÃO INVALIDA, TENTE NOVAMENTE! ")
