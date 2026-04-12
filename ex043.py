e2ed8a8c6b37610b87c8b55c13a68ad6762ccc86
GxsttDev <gustavogomes3035@gmail.com>
2026-04-12 20:43:24 -0300

    Atualizei tal arquivo

--- ex043.py
+++ ex043.py
@@ -6,11 +6,11 @@ print (f"Seu IMC é: {IMC:.1f}")
 
 if IMC < 18.5:
     print ("Você esta abaixo do peso ")
-elif IMC < 25:
+elif 18.5 <= IMC < 25:
     print ("Você esta no peso ideal ")
-elif IMC < 30:
+elif 25 <= IMC < 30:
     print ("Você esta acima do peso ")
-elif IMC < 40:
+elif 30 <= IMC < 40:
     print ("Você esta com obesidade ")
 else:
     print ("Você esta com obesidade morbida, cuide da sua saude! ")
