f882b46f92a1cf8107c1b7e6f183927e05dde6fb
GxsttDev <gustavogomes3035@gmail.com>
2026-04-12 21:47:28 -0300

    Atualizando arquivo ex044.py

--- ex044.py
+++ ex044.py
@@ -1,5 +1,5 @@
 PrecoNormal = int(input("Digite o preço do produto: "))
-print ("===== LOJA DO GUSTAVO =====")
+print ("{:=^40}".format("LOJA GUSTAVO"))
 print ("[1] Dinheiro ou Cheque: ")
 print ("[2] A vista no Cartão ")
 print ("[3] 2x no Cartão ")
@@ -8,14 +8,14 @@ Pagamento = int(input("Qual sua opção: "))
 
 if Pagamento == 1:
     PrecoFinal = PrecoNormal * 0.90
-    print (f"Sua compra de {PrecoNormal}, vai custar {PrecoFinal}")
+    print (f"Sua compra de {PrecoNormal}, vai custar {PrecoFinal:.2f}")
 elif Pagamento == 2:
     PrecoFinal = PrecoNormal * 0.95
-    print (f"Sua compra de {PrecoNormal}, vai custar {PrecoFinal}")
+    print (f"Sua compra de {PrecoNormal}, vai custar {PrecoFinal:.2f}")
 elif Pagamento == 3:
     print (f"Sua compra vai custar: {PrecoNormal}")
 elif Pagamento == 4:
     PrecoFinal = PrecoNormal * 1.20
-    print (f"Com o juros da parcela sua compra custara: {PrecoFinal}")
+    print (f"Com o juros da parcela sua compra custara: {PrecoFinal:.2f}")
 
 print ("===== OBRIGADO, VOLTE SEMPRE =====")
