7e7da4d8e92a7cc30ed6e2f0922fb5f0a4adf00b
GxsttDev <gustavogomes3035@gmail.com>
2026-04-15 20:04:51 -0300

    atualizacao ex045

--- ex045.py
+++ ex045.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 
 itens = ("Pedra", "Papel", "Tesoura")
 Computador = randint(0, 2)
@@ -7,6 +8,11 @@ print ("""Suas Opcões
 [1] Papel
 [2] Tesoura  """)
 Jogador = int(input("Qual sua jogada: "))
+print ("JO")
+sleep (1)
+print ("KEN")
+sleep (1)
+print ("PO")
 print ("-=" * 12)
 print (f"Computador jogou {itens[Computador]}")
 print (f"Jogador jogou {itens[Jogador]}")
