5435279f4f40c1c7f3cb235e1220ae0fb5d6c4ac
GxsttDev <gustavogomes3035@gmail.com>
2026-05-27 20:18:44 -0300

    Adicionando exercícios

---
+++ exercicio4.py
@@ -0,0 +1,7 @@
+N1 = int(input("Digite um numero: "))
+N2 = int(input("Digite outro numero>: "))
+S = N1 + N2
+med = (N1 + N2) / 2
+
+print ("A soma entre {} e {} é igual a {}".format(N1,N2,S))
+print ("E a media entre elas é igual a {}".format(med))
